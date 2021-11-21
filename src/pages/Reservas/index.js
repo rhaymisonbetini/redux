b273603479a9852ae0fd1249d72ab6989ad8dd69
RHAYMISON <rhaymison@hotmail.com>
2021-11-21 09:13:07 -0300

    att

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,31 +1,38 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { MdDelete } from 'react-icons/md';
 import './style.css';
 
 export default function Reservas() {
- return (
-   <div>
-    <h1 className="title">Voce solicitou 1 reservas</h1>
 
-    <div className="reservas">
-      <img
-      src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"
-      alt="Maceio"
-      />
-      <strong>Viagem Maceio 7 Dias</strong>
-      <span>Quantidade: 2</span>
-      <button
-      type="button"
-      onClick={()=> {}}
-      >
-        <MdDelete size={20} color="#191919" />
-      </button>
+  const reservers = useSelector(state => state.reserve)
+
+  return (
+    <div>
+      <h1 className="title">Voce solicitou {reservers.length} {reservers.length === 1 ? 'reserva' : 'reservas'}</h1>
+      {
+        reservers.map(reserve => (
+          <div className="reservas" key={reserve.id}>
+            <img
+              src={reserve.image}
+              alt={reserve.title}
+            />
+            <strong>{reserve.title}</strong>
+            <span>Quantidade: {reserve.amount}</span>
+            <button
+              type="button"
+              onClick={() => { }}
+            >
+              <MdDelete size={20} color="#191919" />
+            </button>
+          </div>
+
+        ))
+      }
+      <footer>
+        <button type="button">Solicitar Reservas</button>
+      </footer>
+
     </div>
-
-    <footer>
-      <button type="button">Solicitar Reservas</button>
-    </footer>
-
-   </div>
- );
+  );
 }
